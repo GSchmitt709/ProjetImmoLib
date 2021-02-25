5994147d7682e16d1d95dd00455201c894f5d8bb
GSchmitt709 <sioschmitt@gmail.com>
2021-02-25 11:33:02 +0100

    premiere passe de DB

---
+++ ProjetImmoLib/Controllers/brokerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetImmoLib.Controllers
+{
+    public class brokerController : Controller
+    {
+        // GET: broker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
